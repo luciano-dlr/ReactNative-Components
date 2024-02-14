59a1e711858269c32c83bd5ffddd7b2b75bacdec
luciano-dlr <delarubialuciano@gmail.com>
2024-02-14 20:06:02 -0300

    animation testing

---
+++ src/hooks/useAnimation.tsx
@@ -0,0 +1,98 @@
+import { useRef } from "react"
+import { Animated, Easing } from "react-native"
+
+
+export const useAnimation = () => {
+
+    const opacity  = useRef(new Animated.Value(0)).current
+    const position = useRef(new Animated.Value(0)).current
+    const position2 = useRef(new Animated.Value(0)).current
+
+
+
+    const fadeIn = () => {
+        Animated.timing(
+            opacity,
+            {
+                toValue: 1,
+                duration: 300,
+                useNativeDriver: true
+            }
+        ).start(() => {
+            console.log('termino la animacion');
+        })
+
+
+    
+
+    }
+
+
+    const fadeOut = () => {
+        Animated.timing(
+            opacity,
+            {
+                toValue: 0,
+                duration: 300,
+                useNativeDriver: true
+            }
+        ).start()
+        Animated.timing(
+            position, {
+            toValue: -300,
+            duration: 1500,
+            useNativeDriver: true,
+            easing: Easing.bounce,
+        }
+        ).start()
+    }
+
+
+    const startMovingPosition = (initPosition:number,duration:number = 300 ) => {
+
+        position.setValue(-300)
+
+        
+        Animated.timing(
+            position, {
+            toValue:0,
+            duration,
+            useNativeDriver: true,
+            easing: Easing.bounce
+        }
+        ).start()
+
+    }
+    const finishMovingPosition = (initPosition:number,duration:number = 1500 ) => {
+
+        position2.setValue(-300)
+
+        
+        Animated.timing(
+            position2, {
+            toValue:-300,
+            duration,
+            useNativeDriver: true,
+            easing: Easing.bounce
+        }
+        ).start()
+
+    }
+
+   
+
+
+
+    return {
+        opacity,
+        fadeIn,
+        fadeOut,
+        position,
+        position2,
+        startMovingPosition,
+        finishMovingPosition
+
+    }
+}
+
+
--- src/screens/Animation101Screen.tsx
+++ src/screens/Animation101Screen.tsx
@@ -1,20 +1,64 @@
 import React, { useRef } from 'react'
-import { Animated, StyleSheet, View } from 'react-native'
+import { Animated, Easing, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { useAnimation } from '../hooks/useAnimation'
+
 
 export const Animation101Screen = () => {
 
-    const opacity = useRef(new Animated.Value(0.4)).current
+    const {opacity,position,fadeIn,fadeOut,startMovingPosition,position2,finishMovingPosition} = useAnimation()
 
+    const handleFadeIn = () => {
+        fadeIn();
+        startMovingPosition(-300, 1300);
+    };
 
+    const handleFadeOut = () => {
+        fadeOut();
+        // finishMovingPosition(0)
+    }
 
-
+    // <Nachovski></Nachovski> sensei original 
 
     return (
         <View style={styles.container}>
             <Animated.View style={{
                 ...styles.purpleBox,
-                opacity: opacity
+                opacity,
+                transform:[{
+                    translateY:position,
+                    
+                    
+                },
+                {
+                    translateX:position2
+                }
+            
+            
+            ]
             }} />
+
+
+            <View style={styles.btnContainer}>
+                <TouchableOpacity
+
+                    onPress={handleFadeIn}
+                    style={styles.btn}
+                >
+                    <Text style={styles.btnText}> FadeIn</Text>
+
+                </TouchableOpacity>
+
+                <TouchableOpacity
+
+                    onPress={handleFadeOut}
+                    style={{...styles.btn,backgroundColor:'#A16868'}}
+                >
+                    <Text style={styles.btnText}> FadeOut</Text>
+
+                </TouchableOpacity>
+
+            </View>
+
         </View>
     )
 }
@@ -24,14 +68,28 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
-
+        alignItems: 'center',
+        backgroundColor:'#CCCCCC'
     },
-
     purpleBox: {
         backgroundColor: '#5856D6',
-        width: 150,
-        height: 150
+        width: 220,
+        height: 200,
+        borderRadius:12
     },
+    btnContainer: {
+        flexDirection:'row'
+    },
+    btn: {
+        padding: 20,
+        margin:20,
+        borderRadius:9,
+        backgroundColor: '#729E72'
+    },
+    btnText:{
+        fontSize:14,
+        fontWeight:'bold',
+        color:'white'
+    }
 
 });
